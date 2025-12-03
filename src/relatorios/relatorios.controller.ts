436821d0fe0cbfb16875616f6bcc0e5c6bd30633
kaiquerennan <kaiquerennan@gmail.com>
2025-12-03 17:32:18 -0300

    quantidade por categoria

--- src/relatorios/relatorios.controller.ts
+++ src/relatorios/relatorios.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
-import { RelatoriosService } from './relatorios.service';
+import { Controller, Get } from '@nestjs/common'
+import { RelatoriosService } from './relatorios.service'
 
 @Controller('relatorios')
 export class RelatoriosController {
   constructor(private readonly relatoriosService: RelatoriosService) {}
-}
+
+    @Get('vendas-por-categoria')
+      getVendasPorCategoria() {
+        return this.relatoriosService.vendasPorCategoria();
+      }
+
+  }
+
--- src/relatorios/relatorios.service.ts
+++ src/relatorios/relatorios.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+
 
 @Injectable()
-export class RelatoriosService {}
+
+  export class RelatoriosService {
+    constructor(private prisma: PrismaService){}
+
+    async vendasPorCategoria() {
+      const vendasPorCategoria = await this.prisma.$queryRaw<any[]>`
+        SELECT p.CATEGORIA as categoria, 
+               SUM(iv.quantidade)::text as quantidade_vendidas
+        FROM "itens_vendas" iv INNER JOIN "Produto" p
+        ON iv."produtoId" = p.id GROUP BY p.CATEGORIA LIMIT 5
+      `;
+
+      return vendasPorCategoria
+    }
+  }
